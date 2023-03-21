cabe0692cf48990d4b85d9c15ec77e50ec27b049
Darya Vorobeva <d.vorobieva15@gmail.com>
2023-03-21 16:06:00 +0300

    added formatted output Program.cs

--- ex/Program.cs
+++ ex/Program.cs
@@ -14,14 +14,27 @@
 
 void PrintArray(string[] array)
 {
-    Console.WriteLine("[{0}]", string.Join(", ", array));
+    if(array.Length == 0) Console.Write("[]");
+    else Console.Write("[{0}]", "\"" + string.Join("\", \"", array) + "\"");
 }
 
 string[] array1 = new string[] { "hello", "2", "world", ":-)" };
 string[] array2 = new string[] { "1234", "1567", "-2", "computer science" };
 string[] array3 = new string[] { "Russia", "Denmark", "Kazan" };
 
-PrintArray(SampleThreeChar(array1));
-PrintArray(SampleThreeChar(array2));
-PrintArray(SampleThreeChar(array3));
+string[] result1 = SampleThreeChar(array1);
+string[] result2 = SampleThreeChar(array2);
+string[] result3 = SampleThreeChar(array3);
 
+PrintArray(array1);
+Console.Write(" --> ");
+PrintArray(result1);
+Console.WriteLine();
+PrintArray(array2);
+Console.Write(" --> ");
+PrintArray(result2);
+Console.WriteLine();
+PrintArray(array3);
+Console.Write(" --> ");
+PrintArray(result3);
+Console.WriteLine();
